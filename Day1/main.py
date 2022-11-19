11a8618bae62f4b272c6cafb6b349555d8a9fedf
Koen Oosterwijk <koen.oosterwijk@demcon.com>
2022-11-19 14:30:23 +0100

    Implemented sliding window

--- Day1/main.py
+++ Day1/main.py
@@ -10,14 +10,17 @@ def Day1():
 
     df = LoadCsvFile()
     #print(df.to_string())
-    increasements = calculateDeptIncreasement(df)
-    print(f'amount of increasements is {increasements}')
+    #increasements = calculateDeptIncreasement(df)
+    #print(f'amount of increasements is {increasements}')
+    slidingWindow = calculateDeptIncreasementSlidingWindow(df)
+    print(f'amount of increasements is {slidingWindow}')
 
 def LoadCsvFile():
     try:
         return pd.read_csv("input.txt", names=['Dept'])
     except:
         print('Error reading input file')
+
 
 def calculateDeptIncreasement(df):
     increaseCounter = 0
@@ -29,6 +32,19 @@ def calculateDeptIncreasement(df):
             increaseCounter = increaseCounter + 1
        comparedept = int(df.iloc[index])
        index = index + 1
+    return increaseCounter
+
+def calculateDeptIncreasementSlidingWindow(df):
+    increaseCounter = 0
+    index = 0
+    comparedept = int(df.iloc[0]) + int(df.iloc[1]) + int(df.iloc[2])
+    for i, row in df.iterrows():
+        if index < (df.size - 2):
+            if int(df.iloc[index]) + int(df.iloc[index + 1]) + int(df.iloc[index + 2]) > comparedept:
+                print(f'dept {int(df.iloc[index])} is bigger then previous dept {comparedept}')
+                increaseCounter = increaseCounter + 1
+            comparedept = int(df.iloc[index]) + int(df.iloc[index + 1]) + int(df.iloc[index + 2])
+            index = index + 1
     return increaseCounter
 
 # Press the green button in the gutter to run the script.
